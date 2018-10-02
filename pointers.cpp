5fea152538af1a0a0036c84788eac4e76fa663cf
RritikaG <42543410+RritikaG@users.noreply.github.com>
2018-10-02 08:22:11 +0530

    Updated pointers

--- pointers.cpp
+++ pointers.cpp
@@ -9,10 +9,10 @@ int mul(int x,int y)
 {return x*y;}
 int main()
 {
-int (*p[3])(int ,int);
+int (*p[3])(int ,int);//defining function pointer
 p[0]=add;
 p[1]=sub;
 p[2]=mul;
 for(int i=0;i<3;i++)
-cout<<p[i](5,6)<<endl;
+cout<<p[i](5,6)<<endl;//calling different functions using function pointer
 }
